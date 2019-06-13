c0546bddbee78fae32a29bdc86878a6e4caab062
RIscRIpt <richard@riscript.com>
2019-06-13 23:30:29 +0200

    fix link error in third-party project

--- random.cpp
+++ random.cpp
@@ -7,7 +7,7 @@
 using namespace rrl;
 using namespace rrl::rlc;
 
-Bytes random(size_t n) {
+Bytes rrl::rlc::random(size_t n) {
     Bytes result(n);
     if (!RAND_bytes(reinterpret_cast<unsigned char*>(result.data()), n))
         throw Exception(ERR_get_error());
